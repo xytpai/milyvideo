9bd00a562366fb6e0f267a058c9513d3a03307e1
xytpai <xytpai@foxmail.com>
2023-01-29 15:32:04 +0800

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # milyvideo
+
+MilyVideo is a homemade open source video website.
+
+#### 1. Connect to database
+
+```txt
+sudo mysql
+-> create database milyvideo;
+-> exit;
+python manage.py makemigrations
+python manage.py migrate
+```
--- milyvideo/settings.py
+++ milyvideo/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'milyvideo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'milyvideo', # cat /etc/mysql/debian.cnf
+        'USER': 'debian-sys-maint', 
+        'PASSWORD': 'qqdWvUpyYdfW9crD', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
     }
 }
 
@@ -121,6 +125,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
